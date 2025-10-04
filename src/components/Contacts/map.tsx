389f71ea6cb44a9091c18dd2be8846cf25eeef1f
AshifAli007 <theashifkhan@gmail.com>
2025-10-04 14:01:08 -0400

    map added

--- src/components/Contacts/map.tsx
+++ src/components/Contacts/map.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { Map, Marker, Overlay } from "pigeon-maps";
+import { Map, Marker, Overlay, GeoJsonLoader } from "pigeon-maps";
 import { useState } from "react";
+
 
 type Props = { height?: number; className?: string };
 
 export default function FooterMapLite({ height = 260, className = "" }: Props) {
   const FIXED_CENTER: [number, number] = [30.447463, -84.309007]; // [lat, lng]
   const [center] = useState<[number, number]>(FIXED_CENTER);
+    const geoJsonLink = "https://raw.githubusercontent.com/danielcs88/fl_geo_json/refs/heads/master/geojson-fl-counties-fips.json"
 
 
   const provider = (x: number, y: number, z: number, dpr?: number) =>
@@ -34,6 +36,14 @@ export default function FooterMapLite({ height = 260, className = "" }: Props) {
         defaultCenter={center}
         
       >
+        <GeoJsonLoader
+        link={"geojson-fl.json"}
+        styleCallback={(feature, hover) =>
+          hover
+            ? { fill: 'rgba(0, 160, 175, 0.73)', strokeWidth: '2', stroke: '#ffffffcb' }
+            : { fill: 'rgba(137, 211, 206, 0.5)', strokeWidth: '1', stroke: '#0998c353' }
+        }
+      />
         <Marker width={36} anchor={center} />
         <Overlay anchor={center} offset={[20, -3]}>
           <div className="px-2 py-1 rounded bg-black/60 text-white text-xs font-medium shadow">
